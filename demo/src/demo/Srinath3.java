d8ef1cf4709035b56ceab4b7081f2c9c4cf2f532
hp <hp@PAVANTEJ>
2022-06-05 13:09:56 +0530

    first commit

---
+++ demo/src/demo/Srinath3.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Srinath3 {
+
+	public static void main(String[] args) {
+		int number=8;
+		double $=number;
+		System.out.println( $);
+		System.out.println( number);
+		
+
+	}
+
+}
