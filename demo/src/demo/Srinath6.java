5735183b8f67e3026cc723c05018788266735be5
hp <hp@PAVANTEJ>
2022-06-05 13:31:47 +0530

    first commit

---
+++ demo/src/demo/Srinath6.java
@@ -0,0 +1,22 @@
+package demo;
+
+public class Srinath6 {
+	static void srinath6(float x) {
+		System.out.println("float");
+		
+	}
+	static void srinath6(double x) {
+		System.out.println("double");
+	}
+	
+
+	public static void main(String[] args) {
+		srinath6(99.7);
+	
+		
+		
+		
+
+	}
+
+}
