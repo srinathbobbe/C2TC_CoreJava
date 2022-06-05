2677aebd14451879779347327107a0273e8fd850
hp <hp@PAVANTEJ>
2022-06-05 13:14:51 +0530

    first commit

---
+++ demo/src/demo/Srinath4.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Srinath4 {
+
+	public static void main(String[] args) {
+		double size=10;
+		float point=(float)size;
+		System.out.println(size);
+		System.out.println(point);
+		
+
+	}
+
+}
